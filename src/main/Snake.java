72d1d72c416833c394d0f7be89c3a7176247edc3
leowrites <75044178+leowrites@users.noreply.github.com>
2020-12-15 20:28:15 -0800

    Update 12-15

--- src/main/Snake.java
+++ src/main/Snake.java
@@ -3,10 +3,16 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Snake extends JFrame {
+    /*
+    Snake
+    Started: 2020-12-14
+    Completed: 2020-12-15
+    Leo Liu
+     */
     // Launcher of the game
 
     Snake(){
-
+        super("Snake By Leo");
         Dimension dimension = new Dimension(800, 800);
         setSize(dimension);
         setLocationRelativeTo(null);
